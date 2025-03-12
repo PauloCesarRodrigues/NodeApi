e092fa2e8035f8c8ab3c8f5fb145ceb3e43737fb
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-03-12 10:17:55 -0300

    feature: json db creation

--- src/server.js
+++ src/server.js
@@ -1,25 +1,37 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
 
 
-const users = []
+const database = new Database
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer(async (req, res)=>{
 
   const {method, url} = req
 
+ 
+  await json(req, res)
+
   if (method === 'GET' && url === '/users'){
-    return res
-    .setHeader('Content-type', 'application/json')
-    .end('Rota de listagem de usuários' + JSON.stringify(users))
+
+    const users = database.select('users')
+    return res.end('Rota de listagem de usuários' + JSON.stringify(users))
   }
 
 
-  if(method === 'POST' && url === '/users'){
-    users.push({
-      id: 1,
-      name: 'carinha q mora logo ali',
-      email: 'emaildocarinha@gmail.com'
-    })
+  if(method === 'POST' && url === '/users'){  
+
+    const {name, email} = req.body
+
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+    }
+
+    database.insert('users', user)
+
     return res.writeHead(201).end()
   }
 
